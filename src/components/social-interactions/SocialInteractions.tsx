6ad0a0799f5a262abc0568059169c0d50f3a00c1
Don Jasa <LyndonJ@magenic.com>
2021-05-10 11:44:14 +0800

    removed unused code

--- src/components/social-interactions/SocialInteractions.tsx
+++ src/components/social-interactions/SocialInteractions.tsx
@@ -1,18 +1,10 @@
-import { Fab, makeStyles } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import useSocialInteraction from "../../hooks/useSocialInteraction";
 import { GroupedCovidDataModel } from "../../models/GroupedCovidDataModel";
 import { groupData } from "../../shared/group-data-helper";
 import CovidDataTable from "../shared/CovidDataTable";
-import AddIcon from '@material-ui/icons/Add';
 import './SocialInteractions.scss';
 import CovidDataAddButton from "../shared/CovidDataAddButton";
-
-const useStyles = makeStyles((theme) => ({
-  extendedIcon: {
-    marginRight: theme.spacing(1)
-  }
-}));
 
 const SocialInteractions = () => {
   const { interactions } = useSocialInteraction();
@@ -22,8 +14,6 @@ const SocialInteractions = () => {
   const [length, setLength] = useState(0);
   const [page, setPage] = useState(0);
   const [take, setTake] = useState(5);
-  
-  const classes = useStyles();
 
   const onRowsPerPageChange = (value: number) => {
     setTake(value);
